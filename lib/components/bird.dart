6c257430eefc21dd69c69b4c3e23638511e37f1a
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-03-25 11:29:18 +0100

    added bird

---
+++ lib/components/bird.dart
@@ -0,0 +1,25 @@
+import 'package:flame/components.dart';
+import 'package:flame_test/game/assets.dart';
+import 'package:flame_test/game/bird_movement.dart';
+import 'package:flame_test/game/flappy_bird_game.dart';
+
+class Bird extends SpriteGroupComponent<BirdMovement>
+    with HasGameRef<FlappyBirdGame> {
+  Bird();
+
+  @override
+  Future<void> onLoad() async {
+    final birdMidFlap = await gameRef.loadSprite(Assets.birdMidFlap);
+    final birdUpFlap = await gameRef.loadSprite(Assets.birdUpFlap);
+    final birdDownFlap = await gameRef.loadSprite(Assets.birdDownFlap);
+
+    size = Vector2(50, 40);
+    position = Vector2(50, gameRef.size.y / 2 - size.y / 2);
+    current = BirdMovement.middle;
+    sprites = {
+      BirdMovement.middle: birdMidFlap,
+      BirdMovement.up: birdUpFlap,
+      BirdMovement.down: birdDownFlap,
+    };
+  }
+}
--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,13 +1,17 @@
 import 'package:flame/game.dart';
 import 'package:flame_test/components/background.dart';
+import 'package:flame_test/components/bird.dart';
 import 'package:flame_test/components/ground.dart';
 
 class FlappyBirdGame extends FlameGame {
+  late Bird bird;
+
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
+      bird = Bird(),
     ]);
   }
 }
